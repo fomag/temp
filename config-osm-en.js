6e2157213af92f90c4bbba8d9325d4085266c4a1
fomag <andrew.tam@cra-arc.gc.ca>
2020-02-11 11:27:31 -0500

    Add files via upload

---
+++ config-osm-en.js
@@ -0,0 +1,16 @@
+/*!
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * v4.0.33 - 2020-01-23
+ *
+ */var wet_boew_geomap = {
+ 	basemap: {
+ 		title:"OSM Map",
+ 		type:"osm",
+ 		mapOptions: {
+ 			center:[-52.7222765,47.5410882],
+ 			maxExtent: '-30.0, -80.0, 40.0, 39.0',
+ 			zoomLevel:11
+ 		}
+ 	}
+ };
